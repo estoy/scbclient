f8016f48a50791b5533d08ee75be3a5d5bfa687a
Erik Stoy <erikstoy@gmail.com>
2017-09-14 19:02:33 +0200

    Add style elements

--- client.elm
+++ client.elm
@@ -1,22 +1,24 @@
 module Client exposing (..)
 
 import Types exposing (..)
-import Html exposing (..)
+import Html exposing (Html)
+import Element exposing (..)
+import Style exposing (..)
 
 
 initialModel : Model
 initialModel =
-    { message = "Hello, World!" }
+    { message = "Hello, Elements World!" }
 
 
 view : Model -> Html msg
 view model =
-    div [] [ text model.message ]
+    viewport (stylesheet []) (text model.message )
 
 
 update : msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    ( model, Cmd.none)
+    ( model, Cmd.none )
 
 
 main : Program Never Model msg
@@ -27,3 +29,10 @@ main =
         , update = update
         , subscriptions = (\model -> Sub.none)
         }
+
+
+sites : List Site
+sites =
+    [ { language = "Svenska", url = "http://api.scb.se/OV0104/v1/doris/sv/ssd" }
+    , { language = "English", url = " http://api.scb.se/OV0104/v1/doris/en/ssd" }
+    ]
